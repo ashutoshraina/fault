95ea05472b91af686d82fe1a651d4753e0f762ff
ashutosh raina <ashutoshraina1989@gmail.com>
2017-09-07 10:34:46 -0700

    initial commit.

---
+++ src/main/java/fault/Application.java
@@ -0,0 +1,53 @@
+package fault;
+
+
+import brave.Tracing;
+import brave.opentracing.BraveTracer;
+import brave.sampler.Sampler;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.okhttp3.TracingInterceptor;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import zipkin.Span;
+import zipkin.reporter.AsyncReporter;
+import zipkin.reporter.Encoding;
+import zipkin.reporter.okhttp3.OkHttpSender;
+
+import java.util.Arrays;
+
+
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public Tracer zipkinTracer() {
+        OkHttpSender okHttpSender = OkHttpSender.builder()
+                .encoding(Encoding.JSON)
+                .endpoint("http://localhost:9411/api/v1/spans")
+                .build();
+        AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
+        Tracing braveTracer = Tracing.newBuilder()
+                .localServiceName("fault")
+                .reporter(reporter)
+                .traceId128Bit(true)
+                .sampler(Sampler.ALWAYS_SAMPLE)
+                .build();
+        return BraveTracer.create(braveTracer);
+    }
+
+    @Bean
+    public OkHttpClient httpClient(@Qualifier("zipkinTracer") Tracer tracer) {
+        TracingInterceptor tracingInterceptor = new TracingInterceptor(tracer, Arrays.asList());
+        return new OkHttpClient.Builder()
+                .addInterceptor(tracingInterceptor)
+                .addNetworkInterceptor(tracingInterceptor)
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/fault/HelloController.java
@@ -0,0 +1,45 @@
+package fault;
+
+import io.opentracing.Tracer;
+import io.opentracing.contrib.okhttp3.RequestBuilderInjectAdapter;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private OkHttpClient client;
+
+    @Qualifier("zipkinTracer")
+    @Autowired
+    private Tracer tracer;
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    @RequestMapping("/chaining")
+    public String chaining() throws IOException {
+
+        String url = "http:localhost:8090/hello";
+
+        Request.Builder builder = new Request.Builder()
+                .url(url);
+        RequestBuilderInjectAdapter injectAdapter = new RequestBuilderInjectAdapter(builder);
+        injectAdapter.put("X-B3-FaultType", "delay");
+        injectAdapter.put("X-B3-Service", "tault");
+
+        Response response = client.newCall(builder.build()).execute();
+        return response.body().string();
+
+    }
+}
